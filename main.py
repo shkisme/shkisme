2bcfa9c75a054b5ab0cd53353694e8886ed38197
손현경 <shkisme0130@gmail.com>
2024-11-14 17:57:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ RSS_FEED = feedparser.parse(URL)
 markdown_text = """
 ## [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fshkisme&count_bg=%23DAB628&title_bg=%232D2B55&icon=github.svg&icon_color=%23E7E7E7&title=GitHub&edge_flat=false)](https://hits.seeyoufarm.com)
 
-<div align = "center">
+<div align="center">
 
 [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=shkisme&rank_icon=github&include_all_commits=true&count_private=true&show_icons=true&theme=shades-of-purple&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage)](https://github.com/anuraghazra/github-readme-stats) 
 [![GitHub Streak](https://streak-stats.demolab.com?user=shkisme&theme=shades-of-purple&card_width=350)](https://git.io/streak-stats)  
@@ -19,18 +19,18 @@ markdown_text = """
 markdown_text += """
 ## 📝 Pinned [Blog](https://shkisme.vercel.app) Posts
 
-<div align = "center">
-  <table style="width: 100%; text-align: center;">
-    <thead>
-      <tr>
-          <th>Title</th>
-          <th>Date</th>
-          <th>Description</th>
-      </tr> 
-    </thead>
-    <tbody>
-"""  # list of blog posts will be appended here
+<table>
+<thead>
+<tr>
+<th>Title</th>
+<th>Date</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+"""
 
+# Add pinned posts
 for idx, feed in enumerate(RSS_FEED['entries']):
     feed_date = time.strftime('%Y/%m/%d', feed['published_parsed'])
     title = feed['title']
@@ -40,41 +40,37 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     summary = soup.find('div', class_='content').text
     isPinned = soup.find('div', class_='isPinned').text
     if isPinned == "false":
-          continue
-  
+        continue
+
     markdown_text += f"""
-      <tr>
-          <td style="width: 33%; padding: 10px;">
-              <a href="{link}">{title}</a>
-          </td>
-          <td style="width: 33%; padding: 10px;">{feed_date}</td>
-          <td style="width: 33%; padding: 10px; text-align: left;">{summary}</td>
-      </tr>
-    """
-    
-markdown_text += """
-    </tbody>
-  </table>
-</div>
+<tr>
+<td><a href="{link}">{title}</a></td>
+<td>{feed_date}</td>
+<td>{summary}</td>
+</tr>
 """
 
 markdown_text += """
+</tbody>
+</table>
+"""
+
+# Add latest posts
+markdown_text += """
 ## 📝 Latest [Blog](https://shkisme.vercel.app) Posts
 
-<div align = "center">
-  <table style="width: 100%; text-align: center;">
-    <thead>
-      <tr>
-          <th>Title</th>
-          <th>Date</th>
-          <th>Description</th>
-      </tr> 
-    </thead>
-    <tbody>
-"""  # list of blog posts will be appended here
+<table>
+<thead>
+<tr>
+<th>Title</th>
+<th>Date</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+"""
 
 MAX_POST = 5
-
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx >= MAX_POST:
         break
@@ -85,21 +81,18 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     description = feed['description']
     soup = BeautifulSoup(description, 'html.parser')
     summary = soup.find('div').find_next('div').text
-  
+
     markdown_text += f"""
-      <tr>
-          <td style="width: 33%; padding: 10px;">
-              <a href="{link}">{title}</a>
-          </td>
-          <td style="width: 33%; padding: 10px;">{feed_date}</td>
-          <td style="width: 33%; padding: 10px; text-align: left;">{summary}</td>
-      </tr>
-    """
-    
+<tr>
+<td><a href="{link}">{title}</a></td>
+<td>{feed_date}</td>
+<td>{summary}</td>
+</tr>
+"""
+
 markdown_text += """
-    </tbody>
-  </table>
-</div>
+</tbody>
+</table>
 """
 
 with open("README.md", mode="w", encoding="utf-8") as f:
