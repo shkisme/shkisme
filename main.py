25e108e698711cdccf144711b9d6cfb33b0da2ba
손현경 <shkisme0130@gmail.com>
2024-11-14 14:19:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,26 @@ URL="https://shkisme.vercel.app/rss.xml"
 RSS_FEED = feedparser.parse(URL)
 
 markdown_text = """
-<a href="https://github.com/shkisme/gitanimals">
-  <img src="https://render.gitanimals.org/lines/shkisme?pet-id=855" width="1000" height="120"/>
-</a>
+## [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fshkisme&count_bg=%23DAB628&title_bg=%232D2B55&icon=github.svg&icon_color=%23E7E7E7&title=GitHub&edge_flat=false)](https://hits.seeyoufarm.com)
 
 <div align = "center">
-  
 [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=shkisme&rank_icon=github&include_all_commits=true&count_private=true&show_icons=true&theme=shades-of-purple&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage)](https://github.com/anuraghazra/github-readme-stats) 
 [![GitHub Streak](https://streak-stats.demolab.com?user=shkisme&theme=shades-of-purple&card_width=350)](https://git.io/streak-stats)  
 </div>
 
 """
 
-markdown_text += """## 📝 Latest [Blog](https://shkisme.vercel.app/) Posts
+markdown_text += """
+## 📝 Latest [Blog](https://shkisme.vercel.app/) Posts
 
-<table style="width: 100%; text-align: center;"><thead>
-<tr>
-    <th>Title</th>
-    <th>Date</th>
-    <th>Description</th>
-</tr>
-</thead><tbody>
+<div align = "center">
+  <table style="width: 100%; text-align: center;"><thead>
+  <tr>
+      <th>Title</th>
+      <th>Date</th>
+      <th>Description</th>
+  </tr>
+  </thead><tbody>
 """  # list of blog posts will be appended here
 
 MAX_POST = 5
@@ -54,7 +53,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     </tr>
     """
 
-markdown_text += "</tbody></table>"
+markdown_text += "</tbody></table></div>"
 
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
